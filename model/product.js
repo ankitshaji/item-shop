b36618db74e5109492dffc334d38a684b5a91076
Ankit Shaji <ankitshaji@gmail.com>
2022-06-16 23:10:58 +1200

    creating product schema and model

---
+++ model/product.js
@@ -0,0 +1,13 @@
+//import mongoose to get schema object
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create a new schema for product
+const product = new Schema({
+        title: String,
+        price:Number,
+        likes:Number
+});
+
+//export after converting schema to model
+module.exports = mongoose.model('Product',product)
