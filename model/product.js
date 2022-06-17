d8e6567290392b1ddde658f36ced42b736661948
Ankit Shaji <ankitshaji@gmail.com>
2022-06-18 06:27:54 +1200

    settings default for likes in product  schema

--- model/product.js
+++ model/product.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 
 //create a new schema for product
 const product = new Schema({
-        title: String,
-        price:Number,
-        likes:Number
+  title: String,
+  price: Number,
+  likes: { type: Number, default: 0 },
 });
 
 //export after converting schema to model
-module.exports = mongoose.model('Product',product)
+module.exports = mongoose.model("Product", product);
